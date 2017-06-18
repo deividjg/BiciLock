4abaa02c4a165ebcc39f42179e73cd30a0337d87
deividjg <deividjg@gmail.com>
2017-06-18 13:24:44 +0200

    Creada nofify activity sin activar

---
+++ app/src/main/java/david/bicilock/NotifyActivity.java
@@ -0,0 +1,40 @@
+package david.bicilock;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class NotifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_notify, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.sendNotify) {
+
+        }
+        if (id == R.id.cancelNotify) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
